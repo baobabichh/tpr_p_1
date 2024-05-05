020b3c3fd5f9fd260901129a4e45959d64a5b17f
baobabichh <pickup2legs@gmail.com>
2024-05-05 14:50:54 +0300

    rm inline

--- Semaphore.h
+++ Semaphore.h
@@ -7,22 +7,9 @@ class Semaphore
 {
 public:
     explicit Semaphore(size_t counter = 0);
-    inline void wait()
-    {
-        std::unique_lock<std::mutex> lk(_m);
-        _cv.wait(lk, [this] { return _counter > 0; });
-        --_counter;
-    }
-    inline void post()
-    {
-        std::unique_lock<std::mutex> lk(_m);
-        ++_counter;
-        _cv.notify_one();
-    }
-    inline size_t available() const
-    {
-        return _counter;
-    }
+    void wait();
+    void post();
+    size_t available() const;
 
 private:
     size_t _counter{};
