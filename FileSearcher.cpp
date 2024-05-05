89b5308782b21c36229272a52cd5ed44ad698192
baobabichh <pickup2legs@gmail.com>
2024-05-05 13:56:41 +0300

    added class FileSearcher

---
+++ FileSearcher.cpp
@@ -0,0 +1,142 @@
+#include "FileSearcher.h"
+
+#include <filesystem>
+
+void FileSearcher::setFileName(const std::string& file_name)
+{
+	_file_name = file_name;
+}
+
+void FileSearcher::setNumberOfThreads(const size_t number_of_threads)
+{
+	_number_of_threads = number_of_threads;
+}
+
+void FileSearcher::setStartDirectory(const std::string& start_dir)
+{
+	_start_dir = start_dir;
+}
+
+void FileSearcher::find()
+{
+	_results.clear();
+	_should_stop = false;
+
+	findImpl();
+}
+
+const std::vector<std::string>& FileSearcher::getResults() const
+{
+	return _results;
+}
+
+void FileSearcher::findImpl()
+{
+	_dirs.push(_start_dir);
+
+	std::vector<std::thread> threads{};
+	for (int i = 0; i < _number_of_threads; i++)
+	{
+		threads.emplace_back(std::thread(&workerThread, this));
+	}
+
+	_new_dir_sem.post();
+
+	int64_t free_threads{ _number_of_threads - 1 };
+	while (true)
+	{
+		{
+			_thread_finished_sem.wait();
+			++free_threads;
+		}
+
+		{
+			std::unique_lock lk(_dirs_m);
+			if (_dirs.empty() && free_threads == _number_of_threads)
+			{
+				_should_stop = true;
+				for (size_t i = 0; i < _number_of_threads; i++)
+				{
+					_new_dir_sem.post();
+				}
+				break;
+			}
+			else if (_dirs.size())
+			{
+				free_threads--;
+				_new_dir_sem.post();
+			}
+		}
+	}
+
+	for (auto& thread : threads)
+	{
+		if (thread.joinable())
+		{
+			thread.join();
+		}
+	}
+}
+
+void FileSearcher::workerThread()
+{
+	while (true)
+	{
+		_new_dir_sem.wait();
+
+		if (_should_stop)
+		{
+			return;
+		}
+
+		std::string dir{};
+		{
+			std::unique_lock lk(_dirs_m);
+			dir = _dirs.front();
+			_dirs.pop();
+		}
+
+		try
+		{
+			for (auto const& dir_entry : std::filesystem::directory_iterator{ dir })
+			{
+				if (dir_entry.is_regular_file())
+				{
+					std::string fname = dir_entry.path().filename().string();
+					if (fname == _file_name)
+					{
+						std::string path = dir_entry.path().string();
+						{
+							std::unique_lock lk(_results_m);
+							_results.push_back(path);
+						}
+					}
+				}
+				else if (dir_entry.is_directory())
+				{
+					std::string next_dir = dir_entry.path().string();
+					{
+						std::unique_lock lk(_dirs_m);
+						_dirs.push(next_dir);
+					}
+				}
+			}
+		}
+		catch (...)
+		{
+
+		}
+
+		_thread_finished_sem.post();
+	}
+}
+
+std::vector<std::string> FileSearcher::findFile(const std::string& file_name, const size_t number_of_threads, const std::string& start_dir)
+{
+	FileSearcher searcher{};
+	searcher.setFileName(file_name);
+	searcher.setNumberOfThreads(number_of_threads);
+	searcher.setStartDirectory(start_dir);
+	searcher.find();
+	return searcher.getResults();
+}
---
+++ FileSearcher.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "Semaphore.h"
+#include <vector>
+#include <queue>
+#include <thread>
+
+class FileSearcher
+{
+public:
+	void setFileName(const std::string& file_name);
+	void setNumberOfThreads(const size_t number_of_threads);
+	void setStartDirectory(const std::string& start_dir);
+	void find();
+	const std::vector<std::string>& getResults()const;
+
+	static std::vector<std::string> findFile(const std::string& file_name, const size_t number_of_threads = 4, const std::string& start_dir = "/");
+
+private:
+	void findImpl();
+	void workerThread();
+
+private:
+	std::string _start_dir{ "/" };
+	std::string _file_name{};
+	size_t _number_of_threads{ 4 };
+	bool _should_stop{ false };
+
+	std::vector<std::string> _results{};
+	std::mutex _results_m{};
+
+	std::queue<std::string> _dirs{};
+	std::mutex _dirs_m{};
+
+	Semaphore _new_dir_sem{};
+	Semaphore _thread_finished_sem{};
+};
