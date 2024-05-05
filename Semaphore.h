94acf34bd3d4b76a4a6ab188d3231942da382d0e
baobabichh <pickup2legs@gmail.com>
2024-05-05 13:37:01 +0300

    move private members

--- Semaphore.h
+++ Semaphore.h
@@ -5,14 +5,14 @@
 
 class Semaphore
 {
-private:
-    size_t _counter{};
-    std::mutex _m{};
-    std::condition_variable _cv{};
-
 public:
     explicit Semaphore(size_t counter = 0);
     void wait();
     void post();
     size_t available() const;
+
+private:
+    size_t _counter{};
+    std::mutex _m{};
+    std::condition_variable _cv{};
 };
