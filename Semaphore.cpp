7d9230d4b782282276284c49ed1e4df35e7b4b9c
baobabichh <pickup2legs@gmail.com>
2024-05-05 14:23:09 +0300

    remove inline

--- Semaphore.cpp
+++ Semaphore.cpp
@@ -4,21 +4,21 @@ Semaphore::Semaphore(size_t counter) : _counter{counter}
 {
 }
 
-inline void Semaphore::wait()
+void Semaphore::wait()
 {
     std::unique_lock<std::mutex> lk(_m);
     _cv.wait(lk, [this] { return _counter > 0; });
     --_counter;
 }
 
-inline void Semaphore::post()
+void Semaphore::post()
 {
     std::unique_lock<std::mutex> lk(_m);
     ++_counter;
     _cv.notify_one();
 }
 
-inline size_t Semaphore::available() const
+size_t Semaphore::available() const
 {
     return _counter;
 }
