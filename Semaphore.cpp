cb3e47fb3a71d1281a5961fe6d1ce4d8bd08fe94
baobabichh <pickup2legs@gmail.com>
2024-05-05 14:47:54 +0300

    remove atomic

--- Semaphore.cpp
+++ Semaphore.cpp
@@ -4,20 +4,3 @@ Semaphore::Semaphore(size_t counter) : _counter{counter}
 {
 }
 
-void Semaphore::wait()
-{
-    std::unique_lock<std::mutex> lk(_m);
-    _cv.wait(lk, [this] { return _counter > 0; });
-    --_counter;
-}
-
-void Semaphore::post()
-{
-    ++_counter;
-    _cv.notify_one();
-}
-
-size_t Semaphore::available() const
-{
-    return _counter;
-}
--- Semaphore.h
+++ Semaphore.h
@@ -7,12 +7,25 @@ class Semaphore
 {
 public:
     explicit Semaphore(size_t counter = 0);
-    void wait();
-    void post();
-    size_t available() const;
+    inline void wait()
+    {
+        std::unique_lock<std::mutex> lk(_m);
+        _cv.wait(lk, [this] { return _counter > 0; });
+        --_counter;
+    }
+    inline void post()
+    {
+        std::unique_lock<std::mutex> lk(_m);
+        ++_counter;
+        _cv.notify_one();
+    }
+    inline size_t available() const
+    {
+        return _counter;
+    }
 
 private:
-    std::atomic<size_t> _counter{};
+    size_t _counter{};
     std::mutex _m{};
     std::condition_variable _cv{};
 };
